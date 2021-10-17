ba403ee024f807e94ebeb28f9ad613b98903ec5a
Luiz Almeida <luiz.garra@live.com>
2021-10-17 14:28:27 -0300

    add citys

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -100,203 +100,234 @@
 
 <script>
 import { ref } from "vue";
-  const citysData =  [
-        {
-      "id": 413,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Alpinópolis",
-      "zipCode": "37940000",
-      "coordinates": {
-        "lat": -20.8269331,
-        "long": -46.530051,
-        "rawType": true
-      },
-      "createdAt": "2021-10-01T16:07:57.448Z",
-      "updatedAt": null,
-      "deletedAt": null
+const citysData =  [
+      {
+    "id": 413,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Alpinópolis",
+    "zipCode": "37940000",
+    "coordinates": {
+      "lat": -20.8269331,
+      "long": -46.530051,
+      "rawType": true
     },
+    "createdAt": "2021-10-01T16:07:57.448Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 402,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Guaxupé",
+    "zipCode": "37800000",
+    "coordinates": {
+      "lat": -21.308362,
+      "long": -46.714397,
+      "rawType": true
+    },
+    "createdAt": "2021-07-26T03:56:36.706Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 401,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Nova Resende",
+    "zipCode": "37860000",
+    "coordinates": {
+      "lat": -21.114253,
+      "long": -46.409309,
+      "rawType": true
+    },
+    "createdAt": "2021-07-26T03:55:35.730Z",
+    "updatedAt": "2021-07-26T03:59:29.495Z",
+    "deletedAt": null
+  },
+  {
+    "id": 403,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "São Pedro da União",
+    "zipCode": "37855000",
+    "coordinates": {
+      "lat": -21.13164,
+      "long": -46.6175338,
+      "rawType": true
+    },
+    "createdAt": "2021-08-09T13:20:28.305Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 404,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Bom Jesus da Penha",
+    "zipCode": "37948000",
+    "coordinates": {
+      "lat": -21.0165299,
+      "long": -46.5226644,
+      "rawType": true
+    },
+    "createdAt": "2021-08-09T13:22:22.012Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 405,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Conceição da Aparecida",
+    "zipCode": "37148000",
+    "coordinates": {
+      "lat": -21.0978884,
+      "long": -46.2209487,
+      "rawType": true
+    },
+    "createdAt": "2021-08-24T14:16:02.214Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 406,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Jacuí",
+    "zipCode": "37965000",
+    "coordinates": {
+      "lat": -21.0165804,
+      "long": -46.7517978,
+      "rawType": true
+    },
+    "createdAt": "2021-09-08T23:58:53.833Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 408,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Alterosa",
+    "zipCode": "37145000",
+    "coordinates": {
+      "lat": -21.2553137,
+      "long": -46.1525009,
+      "rawType": true
+    },
+    "createdAt": "2021-09-09T00:01:39.851Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 407,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Carmo do Rio Claro",
+    "zipCode": "37150000",
+    "coordinates": {
+      "lat": -20.9703672,
+      "long": -46.1316266,
+      "rawType": true
+    },
+    "createdAt": "2021-09-09T00:00:59.604Z",
+    "updatedAt": "2021-09-09T00:02:44.411Z",
+    "deletedAt": null
+  },
+  {
+    "id": 409,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Areado",
+    "zipCode": "37140000",
+    "coordinates": {
+      "lat": -21.3574035,
+      "long": -46.1610013,
+      "rawType": true
+    },
+    "createdAt": "2021-09-09T00:03:46.041Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  //  {
+  //   "id": 410,
+  //   "country": "Brasil",
+  //   "state": "MG",
+  //   "city": "Passos",
+  //   "zipCode": "37900000",
+  //   "coordinates": {
+  //     "lat": -20.7233071,
+  //     "long": -46.649664,
+  //     "rawType": true
+  //   },
+  //   "createdAt": "2021-09-20T13:57:04.069Z",
+  //   "updatedAt": null,
+  //   "deletedAt": null
+  // },
     {
-      "id": 402,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Guaxupé",
-      "zipCode": "37800000",
-      "coordinates": {
-        "lat": -21.308362,
-        "long": -46.714397,
-        "rawType": true
-      },
-      "createdAt": "2021-07-26T03:56:36.706Z",
-      "updatedAt": null,
-      "deletedAt": null
+    "id": 411,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Itaú de Minas",
+    "zipCode": "37975000",
+    "coordinates": {
+      "lat": -20.7346848,
+      "long": -46.8421638,
+      "rawType": true
     },
+    "createdAt": "2021-09-22T16:11:15.737Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
     {
-      "id": 401,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Nova Resende",
-      "zipCode": "37860000",
-      "coordinates": {
-        "lat": -21.114253,
-        "long": -46.409309,
-        "rawType": true
-      },
-      "createdAt": "2021-07-26T03:55:35.730Z",
-      "updatedAt": "2021-07-26T03:59:29.495Z",
-      "deletedAt": null
+    "id": 412,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Juruaia",
+    "zipCode": "37805000",
+    "coordinates": {
+      "lat": -21.2557176,
+      "long": -46.5879916,
+      "rawType": true
     },
+    "createdAt": "2021-09-29T13:36:41.242Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
     {
-      "id": 403,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "São Pedro da União",
-      "zipCode": "37855000",
-      "coordinates": {
-        "lat": -21.13164,
-        "long": -46.6175338,
-        "rawType": true
-      },
-      "createdAt": "2021-08-09T13:20:28.305Z",
-      "updatedAt": null,
-      "deletedAt": null
+    "id": 414,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "São Sebastião do Paraíso",
+    "zipCode": "37950000",
+    "coordinates": {
+      "lat": -20.9020567,
+      "long": -47.2992683,
+      "rawType": true
     },
-    {
-      "id": 404,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Bom Jesus da Penha",
-      "zipCode": "37948000",
-      "coordinates": {
-        "lat": -21.0165299,
-        "long": -46.5226644,
-        "rawType": true
-      },
-      "createdAt": "2021-08-09T13:22:22.012Z",
-      "updatedAt": null,
-      "deletedAt": null
+    "createdAt": "2021-10-17T17:25:29.558Z",
+    "updatedAt": null,
+    "deletedAt": null
+  },
+  {
+    "id": 415,
+    "country": "Brasil",
+    "state": "MG",
+    "city": "Itamogi",
+    "zipCode": "37955000",
+    "coordinates": {
+      "lat": -21.0787493,
+      "long": -47.0550995,
+      "rawType": true
     },
-    {
-      "id": 405,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Conceição da Aparecida",
-      "zipCode": "37148000",
-      "coordinates": {
-        "lat": -21.0978884,
-        "long": -46.2209487,
-        "rawType": true
-      },
-      "createdAt": "2021-08-24T14:16:02.214Z",
-      "updatedAt": null,
-      "deletedAt": null
-    },
-    {
-      "id": 406,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Jacuí",
-      "zipCode": "37965000",
-      "coordinates": {
-        "lat": -21.0165804,
-        "long": -46.7517978,
-        "rawType": true
-      },
-      "createdAt": "2021-09-08T23:58:53.833Z",
-      "updatedAt": null,
-      "deletedAt": null
-    },
-    {
-      "id": 408,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Alterosa",
-      "zipCode": "37145000",
-      "coordinates": {
-        "lat": -21.2553137,
-        "long": -46.1525009,
-        "rawType": true
-      },
-      "createdAt": "2021-09-09T00:01:39.851Z",
-      "updatedAt": null,
-      "deletedAt": null
-    },
-    {
-      "id": 407,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Carmo do Rio Claro",
-      "zipCode": "37150000",
-      "coordinates": {
-        "lat": -20.9703672,
-        "long": -46.1316266,
-        "rawType": true
-      },
-      "createdAt": "2021-09-09T00:00:59.604Z",
-      "updatedAt": "2021-09-09T00:02:44.411Z",
-      "deletedAt": null
-    },
-    {
-      "id": 409,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Areado",
-      "zipCode": "37140000",
-      "coordinates": {
-        "lat": -21.3574035,
-        "long": -46.1610013,
-        "rawType": true
-      },
-      "createdAt": "2021-09-09T00:03:46.041Z",
-      "updatedAt": null,
-      "deletedAt": null
-    },
-    //  {
-    //   "id": 410,
-    //   "country": "Brasil",
-    //   "state": "MG",
-    //   "city": "Passos",
-    //   "zipCode": "37900000",
-    //   "coordinates": {
-    //     "lat": -20.7233071,
-    //     "long": -46.649664,
-    //     "rawType": true
-    //   },
-    //   "createdAt": "2021-09-20T13:57:04.069Z",
-    //   "updatedAt": null,
-    //   "deletedAt": null
-    // },
-     {
-      "id": 411,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Itaú de Minas",
-      "zipCode": "37975000",
-      "coordinates": {
-        "lat": -20.7346848,
-        "long": -46.8421638,
-        "rawType": true
-      },
-      "createdAt": "2021-09-22T16:11:15.737Z",
-      "updatedAt": null,
-      "deletedAt": null
-    },
-     {
-      "id": 412,
-      "country": "Brasil",
-      "state": "MG",
-      "city": "Juruaia",
-      "zipCode": "37805000",
-      "coordinates": {
-        "lat": -21.2557176,
-        "long": -46.5879916,
-        "rawType": true
-      },
-      "createdAt": "2021-09-29T13:36:41.242Z",
-      "updatedAt": null,
-      "deletedAt": null
-    }
-  ];
+    "createdAt": "2021-10-17T17:26:19.259Z",
+    "updatedAt": null,
+    "deletedAt": null
+  }
+
+];
 
 
 export default {
@@ -395,8 +426,6 @@ export default {
         }
         // this.model = this.localization
        }
-
-
   },
   
 };
